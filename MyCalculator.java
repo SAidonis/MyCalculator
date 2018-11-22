49c0a2111312ca0a8b3b928d100111cb4cb13822
SAidonis <Sofia_aidonis@hotmail.com>
2018-11-22 12:03:22 +0100

    add enterDouble

--- MyCalculator.java
+++ MyCalculator.java
@@ -14,6 +14,22 @@ public class MyCalculator {
   private static double mult(double num1, double num2){
     return num1*num2;
   }
+
+  private static double enterDouble(){
+   Scanner scan = new Scanner (System.in);
+   double num;
+   while(true){
+     try{
+       num = scan.nextDouble();
+       break;
+     } catch (Exception e) {
+       String str = scan.nextLine();
+       System.out.println(str + " is not a number!");
+     }
+   }
+   return num;
+ }
+
     public static void main(String[] args){
 
     }
