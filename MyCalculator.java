23133ab4710d06de8c561057afc76092b98f725d
SAidonis <Sofia_aidonis@hotmail.com>
2018-11-22 11:53:01 +0100

    Implementing div

--- MyCalculator.java
+++ MyCalculator.java
@@ -8,7 +8,9 @@ public class MyCalculator {
   private static double sub(double num1, double num2){
     return num1-num2;
   }
-
+  private static double div(double num1, double num2){
+    return num1*num2;
+  }
     public static void main(String[] args){
 
     }
