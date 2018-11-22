df34419e748e5a850a161b025c185e0a319da913
SAidonis <Sofia_aidonis@hotmail.com>
2018-11-22 11:57:21 +0100

    Implementing multi

--- MyCalculator.java
+++ MyCalculator.java
@@ -9,6 +9,9 @@ public class MyCalculator {
     return num1-num2;
   }
   private static double div(double num1, double num2){
+    return num1/num2;
+  }
+  private static double mult(double num1, double num2){
     return num1*num2;
   }
     public static void main(String[] args){
