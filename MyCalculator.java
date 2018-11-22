86932c69c2ca02402ddcab87b2b368342d029cf4
SAidonis <Sofia_aidonis@hotmail.com>
2018-11-22 11:41:57 +0100

    Implementing sub

--- MyCalculator.java
+++ MyCalculator.java
@@ -5,7 +5,9 @@ public class MyCalculator {
   private static double add(double num1, double num2){
     return num1+num2;
   }
-
+  private static double sub(double num1, double num2){
+    return num1-num2;
+  }
 
     public static void main(String[] args){
 
